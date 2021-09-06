6847f8c2033f86a57c8d18e90955257534dce3c1
Rafael Fernández López <ereslibre@ereslibre.es>
2021-09-07 00:44:00 +0200

    Update

--- demo/main.go
+++ demo/main.go
@@ -22,6 +22,52 @@ func kwctlRun() *demo.Run {
 	r.Setup(cleanupKwctl)
 	r.Cleanup(cleanupKwctl)
 
+	r.Step(demo.S(
+		"Search for a policy in hub.kubewarden.io",
+	), nil)
+
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Pull a policy",
+	), demo.S("kwctl pull registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0"))
+
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Inspect policy",
+	), demo.S("kwctl inspect registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0"))
+
+	r.Step(demo.S(
+		"Request with a letsencrypt-production issuer",
+	), demo.S("bat test_data/valid-ingress.json"))
+
+	r.Step(demo.S(
+		"Evaluate request with a letsencrypt-production issuer",
+	), demo.S(`
+    kwctl run \
+      --settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}' \
+      --request-path test_data/valid-ingress.json \
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq
+  `))
+
+	r.Step(demo.S(
+		"Request with a letsencrypt-staging issuer",
+	), demo.S("bat test_data/invalid-ingress.json"))
+
+	r.Step(demo.S(
+		"Evaluate request with a letsencrypt-staging issuer",
+	), demo.S(`
+    kwctl run \
+      --settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}' \
+      --request-path test_data/invalid-ingress.json \
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq
+  `))
+
 	return r
 }
 
