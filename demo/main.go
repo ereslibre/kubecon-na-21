e8bd52265c85eb18797a598c8be8cac5ee3b663e
Rafael Fernández López <ereslibre@ereslibre.es>
2021-09-07 01:05:01 +0200

    Update

--- demo/main.go
+++ demo/main.go
@@ -48,12 +48,10 @@ func kwctlRun() *demo.Run {
 
 	r.Step(demo.S(
 		"Evaluate request with a letsencrypt-production issuer",
-	), demo.S(`
-    kwctl run \
+	), demo.S(`kwctl run \
       --settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}' \
       --request-path test_data/valid-ingress.json \
-      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq
-  `))
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq`))
 
 	r.Step(demo.S(
 		"Request with a letsencrypt-staging issuer",
@@ -61,12 +59,10 @@ func kwctlRun() *demo.Run {
 
 	r.Step(demo.S(
 		"Evaluate request with a letsencrypt-staging issuer",
-	), demo.S(`
-    kwctl run \
+	), demo.S(`kwctl run \
       --settings-json '{"constrained_annotations": {"cert-manager.io/cluster-issuer": "letsencrypt-production"}}' \
       --request-path test_data/invalid-ingress.json \
-      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq
-  `))
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | jq`))
 
 	return r
 }
@@ -79,6 +75,35 @@ func policyServerRun() *demo.Run {
 	r.Setup(setupKubernetes)
 	r.Cleanup(cleanupKubernetes)
 
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Pull a policy",
+	), demo.S("kwctl pull registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0"))
+
+	r.Step(demo.S(
+		"List policies",
+	), demo.S("kwctl policies"))
+
+	r.Step(demo.S(
+		"Generate Kubernetes manifest",
+	), demo.S(`kwctl manifest \
+      --type ClusterAdmissionPolicy \
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | \
+        yq '.metadata.name = "kubecon-na-21"' | \
+        yq '.spec.settings.constrained_annotations."cert-manager.io/cluster-issuer" = "letsencrypt-production"'`))
+
+	r.Step(demo.S(
+		"Apply Kubernetes manifest",
+	), demo.S(`kwctl manifest \
+      --type ClusterAdmissionPolicy \
+      registry://ghcr.io/kubewarden/policies/safe-annotations:v0.1.0 | \
+        yq '.metadata.name = "kubecon-na-21"' | \
+        yq '.spec.settings.constrained_annotations."cert-manager.io/cluster-issuer" = "letsencrypt-production"' | \
+          kubectl apply -f -`))
+
 	return r
 }
 
@@ -88,9 +113,11 @@ func cleanupKwctl() error {
 }
 
 func setupKubernetes() error {
+	cleanupKwctl()
 	return nil
 }
 
 func cleanupKubernetes() error {
+	cleanupKwctl()
 	return nil
 }
